4313dce69b5248c457f9db4759c5e64ffee9433e
Johanne Demetria <jod.demetria@gmail.com>
2016-09-26 22:07:26 +0800

    C# solution to fb problem.

---
+++ SecretSolution_g_2_1/SecretSolution_g_2_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using SecretSolution_g_2_1.Implementation;
+using SecretSolution_g_2_1.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSolution_g_2_1
+{
+    class Program
+    {
+        private static IAnswer _solver;
+        private const int test_cases = 2;
+        private static int[] tests = { 11, 143 };
+        static void Main(string[] args)
+        {
+            _solver = new DivideLambAnswer();
+            
+            for(int i = 0; i< test_cases; i++)
+            {
+                Console.WriteLine("Test #{0} - Input: {1}, Answer = {2}", i + 1, tests[i], _solver.answer(tests[i]));
+            }
+            Console.ReadKey();
+        }
+    }
+}
